d5f6eb0d0ff1a2d8f5fc155b51dd886dd809dbb7
Matheus Amaral <mam.macacod@gmail.com>
2021-03-06 12:53:32 -0300

    reused code from previous version of RotasDF

---
+++ front/script.js
@@ -0,0 +1,54 @@
+let searchButton;
+
+$(document).ready(function(){
+    searchButton = $("#searchBtn");
+
+    searchButton.click(pesquisar);
+    
+})
+
+function pesquisar() {
+
+    var loc = document.getElementById('origem')
+    var dest = document.getElementById('destino')
+
+    for (i = loc.value; i < dest.length; i++) {
+        document.getElementById("destino").onclick = function() {
+            dest.remove(i);
+        };
+    }
+
+    if (loc.value == '' || dest.value == '') {
+        window.alert('[ERROR] Não é possível pesquisar campos vazios. Verifique os dados e tente novamente!')
+    } else if (dest.value == loc.value) {
+        window.alert('[ERROR] Não é possível pesquisar campos iguais. Verifique os dados e tente novamente!')
+    } else {
+        var $origem = $('#origem');
+        var $destino = $('#destino');
+        var $rota = $('#rotaPesquisada');
+
+        $rota[0].innerHTML = 'A rota selecionada passa pelas seguintes cidades respectivamente: ';
+        console.log($rota);
+        let data = {	
+            origem : $origem[0].value, 
+            destino : $destino[0].value
+        };
+        let url = "http://localhost:8000/";
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            success: (res)=>{
+                let cidades = res.caminho;
+                console.log(cidades[cidades.length-1]);
+                let count = 1;
+                for(let i = cidades.length-1; i >= 0; i--){
+                    $rota[0].innerHTML += (`<h5> <b>${count++}. <b> ${cidades[i]}</h5>`);
+                }
+
+            }
+        });
+    }
+}
+
---
+++ index.js
@@ -0,0 +1,40 @@
+const Graph = require('./classes/graph');
+const cities = require('./data/cities');
+const routes = require('./data/citiesRoutes');
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+app.post('/', (req, res) => {
+    const {origem, destino} = req.body;
+
+    if(req.body) {
+
+        const graph = new Graph();
+
+        cities.forEach(city => {
+            graph.addNode(city[0], city[1], city[2]);
+        });
+        routes.forEach(route => {
+            graph.addEdge(route[0], route[1], route[2]);
+        });
+
+        return res.json({caminho : answer});
+    
+    } else {
+        console.log("deu ruim");
+        return res.send(500);
+    }
+});
+
+app.listen(8000, () => console.log(""));
